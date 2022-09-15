151bfa128499fc3a922faffdf43809c12e31b6ca
camila <camilosadaa@hotmail.com>
2022-09-15 16:38:10 +0200

    Add PCA analyses

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os, requests
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 def download_data(url,fname):
 
@@ -23,3 +24,21 @@ def load_data(fname):
         alldata = np.hstack((alldata, np.load('steinmetz_part%d.npz'%j, allow_pickle=True)['dat']))
     
     return alldata
+
+
+def z_score(X):
+    # X: ndarray, shape (n_features, n_samples)
+    ss = StandardScaler(with_mean=True, with_std=True)
+    Xz = ss.fit_transform(X.T).T
+    return Xz
+
+
+def select_areas(regions,data,barea):
+
+    all_dat_area=[]
+    for region in regions:
+        dat_area = data['spks'][barea==region]
+
+        all_dat_area.append(dat_area)
+
+    return all_dat_area
