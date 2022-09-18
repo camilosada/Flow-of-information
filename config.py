da76c725887db646191876cb2af96ddf22d29b55
camila <camilosadaa@hotmail.com>
2022-09-18 19:57:38 +0200

    Add raster plot

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-REGIONS = ["vis ctx", "thal", "hipp", "other ctx", "midbrain", "basal ganglia", "cortical subplate", "other"]
+REGIONS = ["vis ctx", "thal", "hipp", "other ctx", "midbrain", "basal ganglia", "cortical subplate", "other"] # "other ctx": neurons that were not able to be classified
 REGION_COLORS = ['blue', 'red', 'green', 'darkblue', 'violet', 'lightblue', 'orange', 'gray']
 BRAIN_GROUPS = [["VISa", "VISam", "VISl", "VISp", "VISpm", "VISrl"],  # visual cortex
                 ["CL", "LD", "LGd", "LH", "LP", "MD", "MG", "PO", "POL", "PT", "RT", "SPF", "TH", "VAL", "VPL", "VPM"], # thalamus
                 ["CA", "CA1", "CA2", "CA3", "DG", "SUB", "POST"],  # hippocampal
-                ["ACA", "AUD", "COA", "DP", "ILA", "MOp", "MOs", "OLF", "ORB", "ORBm", "PIR", "PL", "SSp", "SSs", "RSP","TT"],  # non-visual cortex
+                ["ACA", "AUD", "COA", "DP", "ILA", "MOp", "MOs", "OLF", "ORB", "ORBm", "PIR", "PL", "SSp", "SSs", "RSP","TT"],  # non-visual cortex (cortical neurons outside of visual cortex)
                 ["APN", "IC", "MB", "MRN", "NB", "PAG", "RN", "SCs", "SCm", "SCig", "SCsg", "ZI"],  # midbrain
                 ["ACB", "CP", "GPe", "LS", "LSc", "LSr", "MS", "OT", "SNr", "SI"],  # basal ganglia
                 ["BLA", "BMA", "EP", "EPd", "MEA"]  # cortical subplate
